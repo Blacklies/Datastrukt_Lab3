a8155e99617a1528b752c7ced71f2a964a9ad240
ShadowPie <atonderski@gmail.com>
2015-12-09 14:32:36 +0100

    integrated Lab3Help

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,16 @@
+
+import Lab3Help.Lab3File;
+
 public class Test {
 
     public static void main(String[] args) {
+
+        Lab3File fileHelp = new Lab3File();
+        try {
+            fileHelp.readLines("hejhej");
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
         System.out.println("hej");
     }
 }
