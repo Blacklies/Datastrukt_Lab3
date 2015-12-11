626da261a1b4b2370ba93f70776912f791b5ffc6
OlofEkborg <olofekborg@gmail.com>
2015-12-11 15:52:03 +0100

    Merge branch 'master' of github.com:Shad0wPie/Datastrukt_Lab3

--- src/Lab3.java
+++ src/Lab3.java
@@ -7,14 +7,16 @@ import java.util.List;
 public class Lab3 {
     public static void main(String[] args) throws IOException, MalformedData {
         Lab3File f = new Lab3File();
-        List<BStop> stops = f.readStops("src/stops-air.txt");
-        List<BLineTable> lines = f.readLines("src/lines-air.txt");
+        List<BStop> stops = f.readStops(args[0]);
+        List<BLineTable> lines = f.readLines(args[1]);
         Path<String> p = new MyPath(stops, lines);
 
-        p.computePath("SFO", "LAX");
-        System.out.println("Distance: " + p.getPathLength());
+        p.computePath(args[2], args[3]);
+        System.out.println(p.getPathLength());
         Iterator<String> path = p.getPath();
         path.forEachRemaining(System.out::println);
+
+
         p.computePath("SFO", "MIA");
         System.out.println("Distance: " + p.getPathLength());
         path = p.getPath();
