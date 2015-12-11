690b36bcc1fd6a81ea52e035ab4e583b908e3ad8
OlofEkborg <olofekborg@gmail.com>
2015-12-11 10:52:09 +0100

    Merge branch 'master' of github.com:Shad0wPie/Datastrukt_Lab3

--- src/MyPath.java
+++ src/MyPath.java
@@ -2,6 +2,7 @@ import Lab3Help.BLineTable;
 import Lab3Help.BStop;
 import Lab3Help.Path;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,9 +15,13 @@ public class MyPath<E> implements Path<E> {
     private Djikstra<E> djikstra;
 
     public MyPath(List<BStop> stops, List<BLineTable> lines) {
+    ArrayList<String> hplatser = new ArrayList<>();
+        for (BStop stop:stops) {
+            hplatser.add(stop.getName());
+        }
 
-        //magic
-//        network = new Network();
+        network = new Network(hplatser);
+
     }
 
     @Override
